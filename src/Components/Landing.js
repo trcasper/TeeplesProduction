b4565cbbaf124f8f2819f95785c8b33fb2903d91
trcasper <trcasper90@gmail.com>
2020-04-02 12:42:43 -0600

    test commit

---
+++ src/Components/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Landing.scss';
+
+
+class Landing extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="Landing">Landing Test</div>
+        
+        )
+    }
+}
+
+export default Landing;
